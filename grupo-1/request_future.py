3b859889d8c82e8b0dc951c52740b045ac30a92d
Zandor Sabino <zandorsabino@gmail.com>
2022-10-24 22:03:49 -0300

    OPEN SPACE 24 10 22

--- grupo-1/request_future.py
+++ grupo-1/request_future.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from requests_futures.sessions import FuturesSession
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -18,31 +19,45 @@ def get_ano_semestre():
 
 
 # PREPARAÇÕES
-def prepara_demanda_classificacao(content):
+def create_urls_curso(fatecs):
+    urls = []
+    for fatec in fatecs:
+        urls.append({
+            "url": "https://www.vestibularfatec.com.br/classificacao/lista.asp",
+            "data": {"CodFatec": fatec['id_fatec']},
+            "hook": response_hook_cursos,
+            "context": {"nome_fatec": fatec['nome_fatec']}
+        })
+    return urls
+
+
+def parse_fatecs(content):
     # TODO SEPARAR
     # TODO Utilizar CSS Selector
     lista_fatecs = []
     soup = BeautifulSoup(content, "html.parser")
-    ano_semestre = get_ano_semestre()
+    # ano_semestre = get_ano_semestre()
 
-    for option in soup.find_all("option")[1:]:
+    for option in soup.find_all("option")[1:5]:
         id_fatec = option["value"]
         nome_fatec = extrai_nome_fatec(option.text)
-
-        lista_fatecs.append(
-            {
-                "url": "https://www.vestibularfatec.com.br/classificacao/lista.asp",
-                "data": {"CodFatec": id_fatec},
-                "hook": response_hook_cursos,
-            }
-        )
-        lista_fatecs.append(
-            {
-                "url": "https://www.vestibularfatec.com.br/demanda/demanda.asp",
-                "data": {"FATEC": nome_fatec, "ano-sem": ano_semestre},
-                "hook": response_hook_demandas,
-            }
-        )
+        lista_fatecs.append({"id_fatec": id_fatec, "nome_fatec": nome_fatec})
+        # lista_fatecs.append(
+        #     {
+        #         "url": "https://www.vestibularfatec.com.br/classificacao/lista.asp",
+        #         "data": {"CodFatec": id_fatec},
+        #         "hook": response_hook_cursos,
+        #         "context": {"nome_fatec": nome_fatec}
+        #     }
+        # )
+        # lista_fatecs.append(
+        #     {
+        #         "url": "https://www.vestibularfatec.com.br/demanda/demanda.asp",
+        #         "data": {"FATEC": nome_fatec, "ano-sem": ano_semestre},
+        #         "hook": response_hook_demandas,
+        #         "context": {"id_fatec": id_fatec}
+        #     }
+        # )
     return lista_fatecs
 
 
@@ -68,13 +83,16 @@ def prepara_curso(content, id_fatec):
 
 # HOOKS
 def response_hook_fatec(resp, *args, **kwargs):
-    lista_fatecs = prepara_demanda_classificacao(resp.content)
-    response_for_urls(lista_fatecs)
+    lista_fatecs = parse_fatecs(resp.content)
+    # response_for_urls(lista_fatecs)
+    resp.data = lista_fatecs
+    return resp
 
 
-def response_hook_id_cursos(resp, *args, **kwargs):
-    all_resps.append(resp)
-    #TODO add parser
+# Fora do ar, em atualização.
+# def response_hook_id_cursos(resp, *args, **kwargs):
+#     all_resps.append(resp)
+#     #TODO add parser
 
 def response_hook_cursos(resp, *args, **kwargs):
     id_fatec = resp.request.body.split("=")[1]
@@ -84,6 +102,7 @@ def response_hook_cursos(resp, *args, **kwargs):
 
 def response_hook_classificacao(resp, *args, **kwargs):
     #TODO add parser
+    # parser_classificacao(resp.content)
     all_resps.append(resp)
 
 
@@ -91,22 +110,42 @@ def response_hook_demandas(resp, *args, **kwargs):
     #TODO add parser
     all_resps.append(resp)
 
+
+# Parsers
+# def parser_classificacao(content):
+#     classificados = []
+#     classificados_soup = content.find_all('tr')
+#     if classificados_soup:
+#         for linha in classificados_soup[1:]:
+#             classificados.append(linha.find_all("td"))
+
+
+#    return classificados
 # REQUESTS
 def response_for_urls(urls):
     """Asynchronously get response for many urls."""
     # TODO Da para usar a sessão ou context name para tratar errors HTTP?
-    resp = None
+    resps = None
     with FuturesSession(max_workers=75) as session:
-        futures = [
-            session.post(
+        # futures = [
+        #     session.post(
+        #         url["url"], data=url.get("data"), hooks={"response": url.get("hook")}
+        #     )
+        #     for url in urls
+        # ]
+        futures = []
+        for url in urls:
+            future = session.post(
                 url["url"], data=url.get("data"), hooks={"response": url.get("hook")}
             )
-            for url in urls
-        ]
-        resp = [f.result() for f in futures] 
+            futures.append(future)
 
-    return resp
-    # return (f.result() for f in futures)
+        resps = [f.result().data for f in futures] 
+        print('RESP')
+        for resp in resps:
+            pprint(resp)
+            break
+    return resps
 
 
 def main_fatec():
@@ -123,12 +162,16 @@ if __name__ == "__main__":
             "url": "https://www.vestibularfatec.com.br/classificacao/fatec.asp",
             "hook": response_hook_fatec,
         },
-        {
-            "url": "https://www.vestibularfatec.com.br/unidades-cursos/",
-            "hook": response_hook_id_cursos,
-        },
+        # Fora do ar, em atualização.
+        # {
+        #     "url": "https://www.vestibularfatec.com.br/unidades-cursos/",
+        #     "hook": response_hook_id_cursos,
+        # },
     ]
 
-    print(list(response_for_urls(urls)))
+    lista_fatec = response_for_urls(urls)
+    urls_cursos = create_urls_curso(lista_fatec)
+    cursos = response_for_urls(urls_cursos)
+    
     print(f'DURAÇÃO: {time.time() - start} segundos')
     print(len(all_resps))
